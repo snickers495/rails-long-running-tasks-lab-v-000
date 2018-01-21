df6c08b265b98fef5e700a24f6881bcf4af7533c
Lisa Lee <snickers495@gmail.com>
2018-01-21 02:57:45 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -36,12 +36,6 @@ class ArtistsController < ApplicationController
     end
   end
 
-  def upload
-    CSV.foreach(params[:leads].path, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
-    end
-    redirect_to customers_path
-  end
 
   def destroy
     @artist = Artist.find(params[:id])
